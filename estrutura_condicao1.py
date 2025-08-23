bd20fc837b2f1e93ddc210627f3eaf782160e2f0
Taciana Alves <tacistreet@gmail.com>
2025-08-23 16:49:58 -0300

    Add files via upload

---
+++ estrutura_condicao1.py
@@ -0,0 +1,6 @@
+def verificar_temperatura(temperatura):
+    if temperatura <30:
+        print("A Temperatura está amena")
+    else:
+        print("Hoje está fazendo calor")
+verificar_temperatura(29)       
