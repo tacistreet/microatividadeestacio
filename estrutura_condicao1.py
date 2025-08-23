612a658923268aaea07e3c2c56a47f85f5026c9b
Taciana Alves <tacistreet@gmail.com>
2025-08-23 18:28:38 -0300

    Update estrutura_condicao1.py

--- estrutura_condicao1.py
+++ estrutura_condicao1.py
@@ -1,6 +1,7 @@
-def verificar_temperatura(temperatura):
+verificar_temperatura(temperatura):
     if temperatura <30:
         print("A Temperatura está amena")
     else:
         print("Hoje está fazendo calor")
+
 verificar_temperatura(29)       
